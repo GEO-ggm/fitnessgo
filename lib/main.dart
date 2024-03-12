c62540ff2e7e705b00ffce01f68e2f46497acec7
GEO-ggm <118511616+GEO-ggm@users.noreply.github.com>
2024-03-12 23:55:40 +0300

    new file:   assets/Logo.png 	modified:   ios/Runner.xcodeproj/project.pbxproj 	modified:   ios/Runner.xcodeproj/xcshareddata/xcschemes/Runner.xcscheme 	modified:   lib/main.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml 	modified:   test/widget_test.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,146 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'FitnessGO',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.white70),
-        useMaterial3: true,
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Montserrat',
+        
       ),
-      home: const MyHomePage(title: 'FitnessGO DEV'),
+      home: AuthorizationScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class AuthorizationScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  AuthorizationScreenState createState() => AuthorizationScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AuthorizationScreenState extends State<AuthorizationScreen> {
+  final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isPasswordHidden = true;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Здесь ты меняешь свою жизнь, начни с мелочей и ты придешь к большему',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Container(
+        
+        decoration: BoxDecoration(
+          image: DecorationImage(
+          image: AssetImage("assets/back.png"), 
+          fit:BoxFit.cover,)
+          ),
+      padding: EdgeInsets.all(18.0),
+      child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(height:40),
+              Row(
+              mainAxisAlignment: MainAxisAlignment.start, 
+              children: [
+              Image.asset('assets/Logo.png', width: 60, height: 60,),
+              Text(
+                'FitnessGO',
+                style: TextStyle(
+                  fontSize: 45,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              ],
+              ),
+              SizedBox(height: 12),
+              Row(
+              mainAxisAlignment: MainAxisAlignment.start, 
+              children: [  
+                Text('Вход',
+                style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextButton(
+                  onPressed: (){
+                    //TODO: Navigate to Registration
+
+                  },
+                  child: Text('Создать'),
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.grey,
+                    textStyle: TextStyle(fontSize: 23, fontWeight: FontWeight.normal),
+                    
+                  ),
+                )
+              ],
+              ),
+              SizedBox(height: 48),
+              // Phone input field
+              TextField(
+                controller: _phoneController,
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  labelText: 'Телефон',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16),
+              // Password input field
+              TextField(
+                controller: _passwordController,
+                obscureText: _isPasswordHidden,
+                decoration: InputDecoration(
+                  labelText: 'Пароль',
+                  border: OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(_isPasswordHidden
+                        ? Icons.visibility_off
+                        : Icons.visibility),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordHidden = !_isPasswordHidden;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(height: 24),
+              // Login button
+              ElevatedButton(
+                onPressed: () {
+                  // TODO: Implement login logic
+                },
+                child: Text('Войти'),
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 50),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  // TODO: Implement forgot password logic
+                },
+                child: Text('Забыли пароль?'),
+              ),
+              // Create account button
+              
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        
+      );
+      
+    
+    
   }
 }
