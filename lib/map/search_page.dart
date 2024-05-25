c6872b6db8cb40d3d8decd503a6afa808ccaefb4
GEO-ggm <118511616+GEO-ggm@users.noreply.github.com>
2024-05-25 12:13:20 +0300

    Update 25/05

--- lib/map/search_page.dart
+++ lib/map/search_page.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:yandex_mapkit/yandex_mapkit.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart' ;
+
 
 import 'control_button.dart';
 import 'map_page.dart';
